0ecc1c0b427bf57790dd5e382ccb70baf6e74636
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-01-15 21:24:03 -0500

    Lab 1

---
+++ lab1.py
@@ -0,0 +1,19 @@
+# Sample Coding Questions 01 Week 01
+# Cheng Yeh Tsai
+
+array1 = [1, 4, 7, 9]
+print(array1)
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - (( b ** c) // d)) + (a % c) 
+print(e)
+
+temperature = float(32.6)
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = input("Enter your age: ")
+print("Now showing the shop items filtered by age: ", userAge)
